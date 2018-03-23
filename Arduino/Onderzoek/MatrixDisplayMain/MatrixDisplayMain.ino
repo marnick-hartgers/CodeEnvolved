041fc16138a7d48b1062b1ce0eaee0b823369782
Jelmergu <jelmergujelmer@hotmail.com>
2018-03-23 09:46:06 +0100

    Something

--- Arduino/Onderzoek/MatrixDisplayMain/MatrixDisplayMain.ino
+++ Arduino/Onderzoek/MatrixDisplayMain/MatrixDisplayMain.ino
@@ -2,10 +2,21 @@
 
 MatrixDisplayController c;
 
+bool animated = false;
+bool animatedFrame = false;
+
 void setup() {
-  
   c.init();
-  c.setPixel(0,1, true);
+  setTimer(0, 2000, toggleAnimation);
+  setTimer(1, 500, toggleHi);
+}
+
+void toggleAnimation() {  
+  animated = !animated;
+}
+
+void offIcon() {
+  c.setPixel(0,2, true);
   c.setPixel(0,3, true);
   c.setPixel(1,1, true);
   c.setPixel(1,4, true);
@@ -18,9 +29,17 @@ void setup() {
   c.setPixel(3,4, true);
   c.setPixel(4,2, true);
   c.setPixel(4,3, true);
+  
 }
 
 void loop() {
+  c.clear();
+  maintainTimers();
+  if (animated == false) {
+    hi();
+  } else { 
+    offIcon();
+  }
   c.draw();
 }
 
@@ -44,11 +63,26 @@ void drawH() {
   c.setPixel(4,4, true);
   c.setPixel(4,5, true);
   c.setPixel(4,6, true);
-  c.draw();
 }
+
+void drawI() {
+  c.setPixel(2,0, true);
+  c.setPixel(2,1, true);
+  c.setPixel(2,2, true);
+  c.setPixel(2,3, true);
+  c.setPixel(2,4, true);
+  c.setPixel(2,6, true);
+}
+
 void hi() {
-    
+  if (animatedFrame == false) {
+    drawH();
+  } else { 
+    drawI(); 
+  }
+}
 
-
+void toggleHi() {
+  animatedFrame = !animatedFrame;
 }
 
